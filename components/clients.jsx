e968c883ab554f153fd7387dbd40f7213f86d29f
unknown <sharibeast@gmail.com>
2021-06-27 04:38:23 +0300

    v1

--- components/clients.jsx
+++ components/clients.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import camusat from "../public/assets/images/camusat.png";
+import iom from "../public/assets/images/iom.png";
+import tanesco from "../public/assets/images/tanesco.png";
+import toshiba from "../public/assets/images/toshiba.png";
+import nhc from "../public/assets/images/nhc.png";
+import songas from "../public/assets/images/songas.png";
 
 import Marquee from "react-fast-marquee";
 
@@ -11,23 +17,20 @@ export default function clients() {
     },
     {
       id: 2,
-      image: require("../public/assets/images/iom.png"),
-    },
-    {
-      id: 3,
-      image: require("../public/assets/images/tanesco.png"),
-    },
-    {
-      id: 4,
       image: require("../public/assets/images/toshiba.png"),
     },
     {
-      id: 5,
-      image: require("../public/assets/images/nhc.png"),
+      id: 3,
+      image: require("../public/assets/images/songas.png"),
     },
     {
-      id: 6,
-      image: require("../public/assets/images/songas.png"),
+      id: 5,
+      image: require("../public/assets/images/tanesco.png"),
+    },
+
+    {
+      id: 4,
+      image: require("../public/assets/images/nhc.png"),
     },
   ];
 
@@ -37,16 +40,9 @@ export default function clients() {
       <h1 className="text-center inset-y-1 text-custom-blue text-2xl font-bold">
         Trusted by
       </h1>
-      <div className="flex hidden  justify-around space-x-24">
-        <Marquee>
-          {clients.map((client) => {
-            return <Image key={client.id} src={client.image} />;
-          })}
-        </Marquee>
-      </div>
-      <div className="flex md:hidden lg:hidden  flex-col">
+      <div className="flex flex-wrap justify-center">
         {clients.map((client) => {
-          return <Image key={client.id} src={client.image} />;
+          return <Image key={client.id} src={client.image} height={200} />;
         })}
       </div>
     </>
